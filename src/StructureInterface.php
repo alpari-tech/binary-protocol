da86739af1b452a1006c2e450930e4abdedb0d12
Alexander Lisachenko <lisachenko.it@gmail.com>
2018-12-28 18:02:24 +0300

    Add general binary field interfaces

---
+++ src/StructureInterface.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the Alpari Kafka client.
+ *
+ * (c) Alpari
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Alpari\BinaryProtocol;
+
+/**
+ * StructureInterface represents classes that can be encoded as packed structure
+ */
+interface StructureInterface
+{
+    /**
+     * Returns definition of binary packet for the class or object
+     */
+    public static function getScheme(): array;
+}
