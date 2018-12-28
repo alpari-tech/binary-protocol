13f772a61f57a5f73ce111d5b31d5f24dc52e6d7
Alexander Lisachenko <lisachenko.it@gmail.com>
2018-12-28 18:12:03 +0300

    Add collection of UInt fields

---
+++ src/Field/UInt16.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * This file is part of the Alpari BinaryProtocol library.
+ *
+ * (c) Alpari
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Alpari\BinaryProtocol\Field;
+
+use Alpari\BinaryProtocol\Stream\StreamInterface;
+
+/**
+ * Represents an integer between 0 and 2^16-1 inclusive.
+ *
+ * The values are encoded using two bytes in machine-dependent byte order.
+ */
+final class UInt16 extends AbstractField
+{
+    /**
+     * Reads a value of field from the stream
+     *
+     * @param StreamInterface $stream    Instance of stream to read value from
+     * @param string          $fieldPath Path to the field to simplify debug of complex hierarchial structures
+     *
+     * @return mixed
+     */
+    public function read(StreamInterface $stream, string $fieldPath)
+    {
+        $packet = $stream->read(2);
+
+        return unpack('SUINT16', $packet)['UINT16'];
+    }
+
+    /**
+     * Writes the value of field to the given stream
+     *
+     * @param mixed           $value     Field value to write
+     * @param StreamInterface $stream    Instance of stream to write to
+     * @param string          $fieldPath Path to the field to simplify debug of complex hierarchial structures
+     *
+     * @return void
+     */
+    public function write($value, StreamInterface $stream, string $fieldPath): void
+    {
+        $packet = pack('S', $value);
+        $stream->write($packet);
+    }
+
+    /**
+     * Calculates the size in bytes of single item for given value
+     */
+    public function getSize($value = null, string $fieldPath = ''): int
+    {
+        return 2;
+    }
+
+    /**
+     * Returns format for unpacking with unpack() function or null if no direct equivalent for this type
+     *
+     * @see pack() for details about format
+     * @return string|null
+     */
+    public function getFormat(): ?string
+    {
+        return 'S';
+    }
+}
