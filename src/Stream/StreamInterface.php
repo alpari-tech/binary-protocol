ca95dc6a4c5ba187f312b1e880bd7b4ca68fafd3
Alexander Lisachenko <lisachenko.it@gmail.com>
2018-12-28 18:02:57 +0300

    Add general stream interface to read/write data

---
+++ src/Stream/StreamInterface.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * This file is part of the Alpari Kafka client.
+ *
+ * (c) Alpari
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Alpari\BinaryProtocol\Stream;
+
+/**
+ * General stream interface to read/write binary packets
+ */
+interface StreamInterface
+{
+    /**
+     * Writes packet to the stream
+     *
+     * @param string $packet Data to write
+     */
+    public function write(string $packet): void;
+
+    /**
+     * Reads packet from the stream, advance internal pointer
+     *
+     * @param int $packetSize Size of packet to read
+     *
+     * @return string Fetched data, up to $packetSize
+     */
+    public function read(int $packetSize): string;
+}
---
+++ src/Stream/StringStream.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * This file is part of the Alpari Kafka client.
+ *
+ * (c) Alpari
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+
+namespace Alpari\BinaryProtocol\Stream;
+
+/**
+ * Simple string stream implementation that can be used for reading/writing data from/to string buffer
+ */
+class StringStream implements StreamInterface
+{
+    /**
+     * Internal binary buffer
+     */
+    private $buffer;
+
+    /**
+     * String stream constructor.
+     *
+     * @param string $stringBuffer Optional buffer to read from
+     */
+    public function __construct(string $stringBuffer = null)
+    {
+        $this->buffer = $stringBuffer ?? '';
+    }
+
+    /**
+     * Writes packet to the stream
+     *
+     * @param string $packet Data to write
+     */
+    public function write(string $packet): void
+    {
+        $this->buffer .= $packet;
+    }
+
+    /**
+     * Reads packet from the stream, advance internal pointer
+     *
+     * @param int $packetSize Size of packet to read
+     *
+     * @return string Fetched data, up to $packetSize
+     */
+    public function read(int $packetSize): string
+    {
+        $bufferLength = strlen($this->buffer);
+        if ($packetSize > $bufferLength) {
+            throw new \InvalidArgumentException("Not enough data in the buffer, has {$bufferLength} bytes");
+        }
+        $packet       = substr($this->buffer, 0, $packetSize);
+        $this->buffer = substr($this->buffer, $packetSize);
+
+        return $packet;
+    }
+
+    /**
+     * Returns the current buffer, useful for write operations
+     */
+    public function getBuffer(): string
+    {
+        return $this->buffer;
+    }
+
+    /**
+     * Checks if buffer is empty
+     */
+    public function isEmpty(): bool
+    {
+        return $this->buffer === '';
+    }
+}
